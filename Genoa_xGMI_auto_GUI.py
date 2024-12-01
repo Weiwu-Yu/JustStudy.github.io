56047f85557f23f3c671d03a766ad1593fc3d97e
beggin-noob <134684586+beggin-noob@users.noreply.github.com>
2024-12-01 20:57:30 +0800

    Update Genoa_xGMI_auto_GUI.py

--- Genoa_xGMI_auto_GUI.py
+++ Genoa_xGMI_auto_GUI.py
@@ -578,15 +578,15 @@ class App:
         self.password_entry.grid(row=8, column=1, columnspan=2, sticky='ew', padx=5, pady=10)
         self.password_eye_open_image = Image.open(get_path("assets/eye_open.png"))
         self.password_eye_open_image = self.password_eye_open_image.resize(
-            (int(self.password_eye_open_image.width * 0.08), 
-             int(self.password_entry.winfo_height() * 10)),
+            (int(self.password_entry.winfo_width() * 15), 
+             int(self.password_entry.winfo_height() * 15)),
              Image.LANCZOS
         ) # 调整图片大小以适应窗口
         self.password_eye_open_photo = ImageTk.PhotoImage(self.password_eye_open_image)
         self.password_eye_close_image = Image.open(get_path("assets/eye_close.png"))
         self.password_eye_close_image = self.password_eye_close_image.resize(
-            (int(self.password_eye_close_image.width * 0.08), 
-             int(self.password_entry.winfo_height() * 10)),
+            (int(self.password_entry.winfo_width() * 15), 
+             int(self.password_entry.winfo_height() * 15)),
              Image.LANCZOS
         ) # 调整图片大小以适应窗口
         self.password_eye_close_photo = ImageTk.PhotoImage(self.password_eye_close_image)
